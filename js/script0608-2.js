eddc17b5def2a66ad6baab4e70a413e486bc78e4
Denis Kustov <kustov.dv@gmail.com>
2019-07-26 20:57:52 +0700

    0608 added

---
+++ js/script0608-2.js
@@ -0,0 +1,3 @@
+$("#my-progress-bar").width("41%")
+
+// $("#my-progress-bar").width("41%").delay(5000).width("66%")
---
+++ js/script0608.js
@@ -0,0 +1,12 @@
+console.log("Скрипт 0608 прогрузился")
+
+// $("#carouselWithIndicators").carousel();
+
+$("#carouselWithIndicators").carousel({
+    interval: 3000
+})
+
+$("#carouselWithIndicators").carousel({
+    wrap: false
+})
+
